f1e089ea57e749ae2b2b6587373478641da4e2af
slendakowskif <wwx26500@student.warszawa.merito.pl>
2024-10-12 12:57:10 +0200

    Update and rename Main.java to Zajecia-1-12-10-2024.java

---
+++ Zajecia-1-12-10-2024.java
@@ -0,0 +1,50 @@
+//public class Bazgroly {
+//    public static void main(String[] args) {
+//        String text = "Hello World!";
+//        String imie = "Filip";
+//        System.out.println(text);
+//        System.out.println("Siema " + imie);
+//        int liczba1 = 12;
+//        int liczba2 = 13;
+//        int suma = liczba1 + liczba2;
+//        System.out.println("Suma liczby1 i liczby2 to: " + suma);
+//        boolean wartosc = true;
+//        if (wartosc == true){
+//            System.out.println("Wartość to true");
+//        }else {
+//            System.out.println("Wartość to false");
+//        }
+//        int iterator = 1;
+//        while (iterator <= 10) {
+//            System.out.println(iterator);
+//            iterator +=1;
+//        }
+//        int a = 10;
+//        int b = 5;
+//        suma = a + b;
+//        int odejmowanie = a - b;
+//        int mnozenie = a * b;
+//        int dzielenie = a / b;
+//        int modulo  = a % b;
+//        System.out.println("Wynik sumy a + b: " + suma +"\nWynik odejmowania a - b: " + odejmowanie + "\nWynik mnożenia a * b: " + mnozenie + "\nWynik dzielenia a / b: " + dzielenie + "\nWynik modulo a % b:" + modulo);
+//        int inkrementacja = 5;
+//        System.out.println(inkrementacja++); // postinkrementacja
+//        System.out.println(inkrementacja);
+//        System.out.println(++inkrementacja); // preinkrementacja
+//        int dekrementacja = 6;
+//        System.out.println(dekrementacja--);
+//        System.out.println(dekrementacja);
+//        System.out.println(--dekrementacja);
+//        
+//    }
+//}
+
+public class Main {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println( a + " + " + b + " = " + c);
+    }
+
+}
